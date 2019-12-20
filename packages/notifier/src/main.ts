4415951e4740542cc73d45cdb269c886ae664f24
Raphael Pigulla <raphael.pigulla@maibornwolff.de>
2019-12-20 12:41:46 +0100

    Remove notifier

--- packages/notifier/src/main.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-/* istanbul ignore file */
-import {change_notifier} from './change_notifier'
-
-change_notifier(process.argv)
-    .catch(function (error) {
-        process.exitCode = 2
-        console.error(error)
-    })
--- packages/notifier/test/Test.spec.ts
+++
@@ -1,7 +0,0 @@
-import {expect} from 'chai'
-
-describe('The notifier', function () {
-    it('should test things', function () {
-        expect(2 + 2).to.equal(4)
-    })
-})

e1a26650be38aaf0fc3521902559f182cc855087
Raphael Pigulla <raphael@pigul.la>
2020-01-01 13:35:48 +0000

    Update READMEs (#24)
    * Fix prepack script for cli
    * Update READMEs
    * Update package.json files
    * Add test for nest-adapter

---
+++ packages/server/test/unit/infrastructure/logger/adapt_for_nest.spec.ts
@@ -0,0 +1,48 @@
+import {expect} from 'chai'
+import {SinonStubbedInstance} from 'sinon'
+import {LoggerService} from '@nestjs/common'
+
+import {adapt_for_nest} from '@src/infrastructure/logger'
+import {ILogger} from '@src/domain'
+
+import {create_logger_stub} from '@test/util/stub'
+
+describe('adapt_for_nest', function () {
+    const message = 'Log message'
+    const context = 'context'
+
+    let logger_stub: SinonStubbedInstance<ILogger>
+    let adapter: Required<LoggerService>
+
+    beforeEach(async function () {
+        logger_stub = create_logger_stub()
+        adapter = adapt_for_nest(logger_stub)
+    })
+
+    it('should proxy error to error', function () {
+        const trace = 'trace'
+
+        adapter.error(message, trace, context)
+        expect(logger_stub.error).to.have.been.calledOnceWithExactly(message, {trace, context})
+    })
+
+    it('should proxy warn to warn', function () {
+        adapter.warn(message, context)
+        expect(logger_stub.warn).to.have.been.calledOnceWithExactly(message, {context})
+    })
+
+    it('should proxy log to info', function () {
+        adapter.log(message, context)
+        expect(logger_stub.info).to.have.been.calledOnceWithExactly(message, {context})
+    })
+
+    it('should proxy debug to debug', function () {
+        adapter.debug(message, context)
+        expect(logger_stub.debug).to.have.been.calledOnceWithExactly(message, {context})
+    })
+
+    it('should proxy verbose to trace', function () {
+        adapter.verbose(message, context)
+        expect(logger_stub.trace).to.have.been.calledOnceWithExactly(message, {context})
+    })
+})
